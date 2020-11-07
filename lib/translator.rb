b566b7d312dac23f324c37ca0daea2157f5ab852
awosborn <alanturing@example.coclear>
2020-11-07 13:10:08 -0700

    Add full english to braille to library

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,12 +6,30 @@ class Translator
   end
 
   def populate
-    @library['a'] = '0./n','00/n','..'
+    @library['a'] = '0./n', '00/n', '..'
     @library['b'] = '0./n', '0./n', '..'
-  end
-
-  def notreal
-    puts "why can you read me?"
+    @library['c'] = '00/n', '../n', '..'
+    @library['d'] = '00/n', '.0/n', '..'
+    @library['e'] = '0./n', '.0/n', '..'
+    @library['g'] = '00/n', '00/n', '..'
+    @library['h'] = '0./n', '00/n', '..'
+    @library['i'] = '.0/n', '0./n', '..'
+    @library['j'] = '.0/n', '00/n', '..'
+    @library['k'] = '0./n', '../n', '0.'
+    @library['l'] = '0./n', '0./n', '0.'
+    @library['m'] = '00/n', '../n', '0.'
+    @library['n'] = '00/n', '.0/n', '0.'
+    @library['o'] = '0./n', '.0/n', '0.'
+    @library['p'] = '00/n', '0./n', '0.'
+    @library['q'] = '00/n', '00/n', '0.'
+    @library['r'] = '0./n', '00/n', '0.'
+    @library['s'] = '.0/n', '0./n', '0.'
+    @library['t'] = '.0/n', '00/n', '0.'
+    @library['u'] = '0./n', '../n', '00'
+    @library['v'] = '0./n', '0./n', '00'
+    @library['x'] = '00/n', '../n', '00'
+    @library['y'] = '00/n', '.0/n', '00'
+    @library['z'] = '0./n', '.0/n', '00'
   end
 end
 
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -12,9 +12,11 @@ class TranslatorTest < Minitest::Test
   def test_it_exsist_and_has_attributes
     # require 'pry'; binding.pry
     expected = '0./n','00/n','..'
-    
+    expected2 = '00/n', '00/n', '0.'
+
     assert_instance_of Translator, @translator
     assert_equal expected, @translator.library['a']
+    assert_equal expected2, @translator.library['q']
     # assert_equal '0./n','00/n','..', @translator.translate_character('a')
   end
 end
