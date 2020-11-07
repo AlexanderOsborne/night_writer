f0c0f827539ece29f1c7280af96984c476a84b67
awosborn <alanturing@example.coclear>
2020-11-07 12:47:56 -0700

    Add first two letters to library

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,16 +6,19 @@ class Translator
   end
 
   def populate
-    @library["a"] = '0./n','00/n','..'
+    @library['a'] = '0./n','00/n','..'
+    @library['b'] = '0./n', '0./n', '..'
   end
 
   def notreal
     puts "why can you read me?"
   end
+end
+
+
 
   # def translate_character(character)
   #   case character
   #   when ('a')         then charcter.tr('a', '0./n','00/n','..')
   #   end
   # end
-end
