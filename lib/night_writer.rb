51de3a0738ede448977629366bc6f19670310016
awosborn <alanturing@example.coclear>
2020-11-08 11:36:51 -0700

    Add start test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,16 +5,22 @@ class NightWriter
   
   def initialize
     
+    start
   end
 
   def translate
       
   end
+
+  def start
+    require 'pry'; binding.pry
+    io = IO.new
+    io.copy
+    puts io.output
+  end
 end
   
-  # io = IO.new
-  # io.copy
-  # puts io.output
+  
 
   
  
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -6,10 +6,17 @@ class NightWriterTest < Minitest::Test
   
   def setup
     @nightwriter = NightWriter.new
+    ARGV.replace(['message.txt','braille.txt'])
   end
 
   def test_it_exsist
 
     assert_instance_of NightWriter, @nightwriter
+  end
+
+  def test_start
+    start
+    
+    assert_instance_of IO, io
   end
 end
