b4a22d2d4a33b34be71f81ab4d7329fcfbc28f8c
awosborn <alanturing@example.coclear>
2020-11-09 15:30:58 -0700

    Add sort_lines

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -4,13 +4,17 @@ require './lib/translator'
 class NightReader
   attr_reader :io,
               :to_read,
-              :to_write
+              :to_write,
+              :lines,
+              :sorted_lines
 
   def initialize
     @io = IO.new
     @braille_text = @io.to_read
     @english_text = @io.to_write
-    @translator = Translator.new
+    @translator   = Translator.new
+    @lines        = []
+    @sorted_lines = []
   end
   
   def start
@@ -18,7 +22,26 @@ class NightReader
   end
 
   def lines
-    braille_lines = IO.readlines(@braille_text, chomp:true)
+    @lines = IO.readlines(@braille_text, chomp:true)
+  end
+
+  def sort_lines
+    top = ""
+    mid = ""
+    bot = ""
+
+    @lines.each do |line|
+      if @lines.index(line) % 3 == 0
+        top += line
+      elsif @lines.index(line) % 3 == 1
+        mid += line
+      else @lines.index(line) % 3 == 2
+        bot += line
+      end
+      @sorted_lines << top
+      @sorted_lines << mid
+      @sorted_lines << bot
+    end
   end
 end
 
