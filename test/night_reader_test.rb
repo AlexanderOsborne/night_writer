599cee52f68fb0bd93b4c228510800eed814aa25
awosborn <alanturing@example.coclear>
2020-11-09 23:01:20 -0700

    Add output test

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -19,7 +19,7 @@ class NightReaderTest < Minitest::Test
     assert_instance_of NightReader, @nightreader
   end
 
-  def test_line
+  def test_read_line
     
     expected = [".000..000..0..0.0.0.0....00.0.00.0...0..0.00..0.0..00.0.0.0...000..0.00...0...00",
       "0......0.000...0.000.0..00.000.00...0.........00.000.00..000..00..0.00.0......0.",
@@ -40,34 +40,44 @@ class NightReaderTest < Minitest::Test
   end
 
   def test_line_by_char
-    skip
+
     @nightreader.read_lines
     @nightreader.sort_lines
     @nightreader.line_by_char
 
-    expected = [["br", "ai", "ll", "e.", "tx"], [], []]
+    expected = [[".0", "00", "..", "00", "0.", ".0", "..", "0.", "0.", "0.", "0.", "..", ".0", "0.", "0.", "00", ".0", "..", ".0", "..", "0.", "00", "..", "0.", "0.", ".0", "0.", "0.", "0.", "0.", "..", "00", "0.", ".0", ".0", "0.", "..", "0.", "..", "00", "0.", ".0", "..", "00", "0.", "0.", "0.", "..", ".0", "0.", "0.", "00", "..", "00", "0.", "0.", ".0", "00", "..", "00", "0.", "0.", "0.", "0.", "00", ".0", "0.", "0.", ".0"], ["0.", "..", "..", ".0", ".0", "00", "..", ".0", ".0", "00", ".0", "..", "00", ".0", "00", ".0", "0.", "..", "0.", "..", "..", "..", "..", "00", ".0", "00", ".0", "0.", ".0", "00", "..", "00", "..", "0.", "00", ".0", "..", "..", "..", "0.", ".0", "00", "..", "..", ".0", "00", ".0", "..", "00", "00", "..", ".0", "..", "0.", ".0", "00", "00", ".0", "..", "..", "00", "..", "00", "..", "..", "00", ".0", "00", "0."], ["..", "0.", "..", "0.", "0.", "0.", "..", "00", "..", "0.", "0.", "..", ".0", "0.", "0.", "..", "0.", "..", "..", "..", "..", "0.", "..", "..", "0.", ".0", "..", "00", "..", "0.", "..", "0.", "00", "..", "0.", "..", "..", "..", "..", "..", "..", ".0", "..", "0.", "0.", "0.", "..", "..", "0.", "..", "..", "0.", "..", "..", "0.", "0.", "0.", "00", "..", "..", "..", "..", "0.", "..", "..", "0.", "..", "0.", "0."]]
 
     assert_equal expected, @nightreader.scanned_lines
   end
 
-  def test_braille_hash
-    skip
+  def test_index_hash
+  
     @nightreader.read_lines
     @nightreader.sort_lines
     @nightreader.line_by_char
     @nightreader.index_hash
 
-    assert_equal true, @nightreader.braille_hash.has_value?(["0., 00, 0."])
+    assert_equal true, @nightreader.braille_hash.has_value?("0. 00 ..")
   end
 
   def test_translate
-    skip
+
     @nightreader.read_lines
     @nightreader.sort_lines
     @nightreader.line_by_char
     @nightreader.index_hash
 
-    expected = []
-    assert_equal expected @nightreader.translate
+    assert_equal nil, @nightreader.translate
+  end
+
+  def test_output
+
+    equal = "Created original_message.txt containing 69 characters"
+    assert_equal equal, @nightreader.output
+  end
+
+  def word_count
+
+    assert_equal
   end
 end
