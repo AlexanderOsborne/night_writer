d4ffa5226ad3346e10f96e03869ea8ad6883f740
awosborn <alanturing@example.coclear>
2020-11-09 19:00:46 -0700

    Refactor IO initialize

--- lib/io.rb
+++ lib/io.rb
@@ -2,13 +2,18 @@ class IO
     attr_reader :to_read,
                 :to_write,
                 :incoming_text,
-                :output_text
+                :output_text,
+                :text,
+                :incoming_text
     
-    def initialize
-      @to_read = ARGV[0]
-      @to_write = ARGV[1]
+    def initialize(inputfile, outputfile)
+      @to_read = inputfile
+      @to_write = outputfile
+    end
+
+    def read
       @incoming_text = File.open(@to_read, 'r')
-      @output_text = File.new(@to_write, "w")
+      @text = File.read(@incoming_text)
     end
 
     def word_count
@@ -16,25 +21,24 @@ class IO
     end
 
     def output
+      read
       "Created #{@to_write}" + " containing #{word_count} characters"
     end
 
     def copy(text)
+      @output_text = File.new(@to_write, "w")
       @output_text.write(text)
       @output_text.close
     end
 
     def split_by_char
-      text = File.read(@incoming_text)
       group_by_40.split("")
     end
 
     def group_by_40
-      text = File.read(@incoming_text)
-
       counter = 1
       holder = ""
-      text.each_char do |c|
+      @text.each_char do |c|
         holder << c
       if counter % 40 == 0 
         holder << "\n"
--- test/io_test.rb
+++ test/io_test.rb
@@ -6,23 +6,23 @@ require './lib/io'
 class IOTest < Minitest::Test
     
     def setup
-      ARGV.replace(['message.txt','braille.txt'])
-      @io = IO.new
+      @io = IO.new("message.txt", "braille.txt")
+      @io.read
     end
 
-    def test_it_exsist()
+    def test_it_exsist
         
       assert_instance_of IO, @io
     end
 
     def test_word_count
 
-      assert_equal 69, @io.word_count
+      assert_equal 70, @io.word_count
     end
 
     def test_output
 
-      expected = "Created braille.txt" + " containing 69 characters"
+      expected = "Created braille.txt" + " containing 70 characters"
       assert_equal expected, @io.output
     end
 
@@ -35,7 +35,8 @@ class IOTest < Minitest::Test
 
     def test_split_by_char
 
-      expected = ["i", "m", " ", "n", "o", "t", " ", "z", "e", "r", "o", " ", "w", "o", "r", "d", "s", " ", "i", " ", "a", "m", " ", "h", "o", "w", "e", "v", "e", "r", " ", "q", "u", "i", "t", "e", " ", "a", " ", "f", "e", "w", " ", "m", "o", "r", "e", " ", "t", "h", "a", "n", " ", "f", "o", "r", "t", "y", " ", "c", "h", "a", "r", "a", "c", "t", "e", "r", "s"]
+      expected = ["i", "m", " ", "n", "o", "t", " ", "z", "e", "r", "o", " ", "w", "o", "r", "d", "s", " ", "i", " ", "a", "m", " ", "h", "o", "w", "e", "v", "e", "r", " ", "q", "u", "i", "t", "e", " ", "a", " ", "f", "
+        +", "e", "w", " ", "m", "o", "r", "e", " ", "t", "h", "a", "n", " ", "f", "o", "r", "t", "y", " ", "c", "h", "a", "r", "a", "c", "t", "e", "r", "s"]
       assert_equal expected, @io.split_by_char
     end
 
